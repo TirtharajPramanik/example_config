a225036344cc173172efba60f70e49ef27700ed9
Tiraj <tirtharajpramanik555@gmail.com>
2023-06-27 18:32:15 +0530

    conditionally prepend or append mason/bin to path with mason_path option
    This functinality has been implemented in NvChad v2.0

--- chadrc.lua
+++ chadrc.lua
@@ -12,6 +12,13 @@ M.ui = {
   hl_add = highlights.add,
 }
 
+-- Where NvChad should put mason.nvim bin location in your PATH. Can be one of:
+-- - "prepend" (default, Mason's bin location is put first in PATH)
+-- - "append" (Mason's bin location is put at the end of PATH)
+-- - "skip" (doesn't modify PATH)
+---@type '"prepend"' | '"append"' | '"skip"'
+M.mason_path = "prepend"
+
 M.plugins = "custom.plugins"
 
 -- check core.mappings for table structure
